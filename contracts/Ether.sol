236ab074cb9d7e1c9a0b2d10806f7d08c17ac79b
Okoro Godwin Chinedu <m.aishat9890@gmail.com>
2022-10-10 17:47:44 +0100

    initial commit

---
+++ contracts/Ether.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+// This Contrant sends, Receive and store ether 
+contract Ether{
+// state  variable which is public
+	address payable public sender;
+	 address public  reciever; 
+      mapping (address => uint) public balances;
+	
+
+	  constructor(address payable _addr) {
+        sender = _addr;
+    }
+
+
+// Allows for emitting or loggging where ether was sent from, who it was sent to and the amount sent
+	event Sent(address from, address to, uint amount); 
+    event Recieved (address to, uint amount);
+
+//error allows you to give information of the transaction fails
+	error InsufficientBalance(uint requested, uint available);
+
+
+    
+//This function allows you to send ether
+    function send(uint amount) public payable{
+        require(msg.sender == sender);
+      
+        sender.transfer(msg.value);
+        emit Sent(msg.sender, reciever, amount);
+
+        if (amount > balances[msg.sender])
+            revert InsufficientBalance({
+                requested: amount,
+                available: balances[msg.sender]
+            });
+       
+
+    }
+    //This function allows you to deposit ether
+    function recieve() external payable{
+       
+    }
+
+
+}
